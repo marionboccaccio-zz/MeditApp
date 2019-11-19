79a9eaa608eefe3e60738d203340b8fa6c25fcde
Marion Boccaccio <marionboccaccio@MacBook-Air-de-Marion.local>
2019-11-19 12:17:14 +0100

    little thing

--- public/main.js
+++ public/main.js
@@ -2,15 +2,14 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 48.86667, lng: 2.349014 },
-    zoom: 12,
+    zoom: 12
     // infowindow = new google.maps.infoWindow()
     // var request = {
     //     query: ['yoga', 'meditation', 'meditation center', 'yoga classes'],
     //     radius : '5000'
-    
-//   }
+
+    //   }
+  });
 }
-
-
 
 initMap();
