dcffb53a32f03d401bdf39c1483167617537f1ae
Hanna Mohamed <hanna@MacBook-Pro-de-Hanna.local>
2019-11-19 12:08:39 +0100

    map init

--- public/main.js
+++ public/main.js
@@ -1,16 +1,71 @@
 var map;
+var infoWindow;
+
+var request;
+var service;
+var markers = [];
+
 function initMap() {
+  var center = new google.maps.LatLng(48.86667, 2.349014);
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 48.86667, lng: 2.349014 },
-    zoom: 12,
-    // infowindow = new google.maps.infoWindow()
-    // var request = {
-    //     query: ['yoga', 'meditation', 'meditation center', 'yoga classes'],
-    //     radius : '5000'
-    
-//   }
+    center: center,
+    zoom: 12
+  });
+
+  request = {
+    location: center,
+    radius: "1000",
+    name: ["yoga"]
+  };
+  infoWindow = new google.maps.InfoWindow();
+
+  service = new google.maps.places.PlacesService(map);
+
+  service.nearbySearch(request, callback);
+
+  google.maps.event.addListener(map, "click", function(event) {
+    clearResults(markers);
+    const latLng = {
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng()
+    };
+
+    var request = {
+      location: latLng,
+      radius: "1000",
+      name: ["yoga", "meditation"]
+    };
+    service.nearbySearch(request, callback);
+  });
 }
 
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (let i = 0; i < results.length; i++) {
+      markers.push(createMarker(results[i]));
+    }
+  }
+}
 
+function createMarker(place) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
 
-initMap();
+  google.maps.event.addListener(marker, "mouseover", function() {
+    console.log(place.name, place.vicinity);
+    infoWindow.setContent(place.name, place.vicinity);
+    infoWindow.open(map, this);
+  });
+  return marker;
+}
+
+function clearResults(markers) {
+  for (var m in markers) {
+    markers[m].setMap(null);
+  }
+  markers = [];
+}
+
+google.maps.event.addDomListener(window, "load", initMap);
