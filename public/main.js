c1ddfb599ca6f9824de15f847feaf865df0c2c75
Marion Boccaccio <marionboccaccio@MacBook-Air-de-Marion.local>
2019-11-19 16:23:23 +0100

    bold agaain

---
+++ public/main.js
@@ -0,0 +1,78 @@
+var map;
+var infoWindow;
+
+var request;
+var service;
+var markers = [];
+const result1 = document.querySelector(".results-list");
+
+function initMap() {
+  var center = new google.maps.LatLng(48.86667, 2.349014);
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: center,
+    zoom: 12
+  });
+
+  request = {
+    location: center,
+    radius: "1000",
+    name: ["yoga", "meditation"]
+  };
+  infoWindow = new google.maps.InfoWindow();
+
+  service = new google.maps.places.PlacesService(map);
+
+  service.nearbySearch(request, callback);
+
+  google.maps.event.addListener(map, "click", function(event) {
+    clearResults(markers);
+    const latLng = {
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng()
+    };
+
+    var request = {
+      location: latLng,
+      radius: "1000",
+      name: "méditation"
+    };
+    service.nearbySearch(request, callback);
+  });
+}
+
+function callback(results, status) {
+  // console.log(results);
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (let i = 0; i < results.length; i++) {
+      markers.push(createMarker(results[i]));
+    }
+    console.log(results);
+    for (let i = 0; i < results.length; i++) {
+      result1.innerHTML += `<li><span style="font-weight: bold;">${results[i].name}</span> : ${results[i].vicinity}</li>`;
+    }
+  }
+}
+function createMarker(place) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, "mouseover", function() {
+    // console.log(place.name, place.vicinity);
+    infoWindow.setContent(place.name, place.vicinity);
+    infoWindow.open(map, this);
+  });
+  return marker;
+}
+
+function clearResults(markers) {
+  console.log("hey");
+  for (var m in markers) {
+    markers[m].setMap(null);
+  }
+  markers = [];
+}
+
+// console.log(markers);
+google.maps.event.addDomListener(window, "load", initMap);
