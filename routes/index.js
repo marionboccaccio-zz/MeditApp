2c0268fc1919a26716539ee262af100af86aab13
Hanna Mohamed <hanna@MacBook-Pro-de-Hanna.local>
2019-11-20 00:34:09 +0100

    suppression de la liste au clic

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("home");
-});
-
 router.get("/home", (req, res) => {
   res.render("home");
 });
+
+// router.get("/home", (req, res) => {
+//   console.log("here");
+//   res.render("home");
+// });
+
 module.exports = router;
--- routes/studios.js
+++ routes/studios.js
@@ -2,7 +2,6 @@ const express = require("express"); // import express in this module
 const router = new express.Router(); // create an app sub-module (router)
 
 router.get("/studios", (req, res) => {
-  console.log("here");
   res.render("studios");
 });
 
